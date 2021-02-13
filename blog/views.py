3861898b8a342d269a7fe0b361a6abe51486d645
Jatumongkon <60840487+Jatumongkon@users.noreply.github.com>
2021-02-13 21:01:47 +0700

    final

--- blog/views.py
+++ blog/views.py
@@ -2,16 +2,13 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 
-i = 1
-
 def post_list(request):
     return render(request,'blog/post_list.html')  
  
 def tab(request):
         num1 = dict()
         number =int (request.GET.get('num'))
-        for i in range(1,11):
+        for i in range(1,13):
             num1[i] = number*i
 
         return render(request,'blog/tab.html',{"result" : num1})
-        return render(request,'blog/tab.html',{"number" : number})
